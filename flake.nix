de7e2cc356b7b3d7a13d358d0b17eb97f465bc45
Vinícius Müller <vinigm.nho@gmail.com>
2023-06-29 17:34:57 -0300

    First commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "FuzzyAlchemist - A fuzzer for the elixir ecosystem";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          packages = [
+            cargo
+            rustc
+            clippy
+            rust-analyzer
+
+            elixir_1_15
+            nil
+          ];
+        };
+      });
+}
